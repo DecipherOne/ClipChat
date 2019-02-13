873a8fe988a28d647ce607e3b19d8b15e14621b3
Will <willc@decipherone.com>
2019-02-13 14:03:22 -0600

    Setting up some basic templates

--- clipchat/urls.py
+++ clipchat/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from chat_data import views as chat_views
+from django.views.generic import TemplateView
 
 urlpatterns = [
+	path('', TemplateView.as_view(template_name="index.html")),
     path('admin/', admin.site.urls),
+    path('signup/', chat_views.signup, name='signup'),
 ]
