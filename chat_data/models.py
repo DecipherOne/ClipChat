266e4768c63df29275ef2c60f68bcaaa98a6ccaa
Will <willc@decipherone.com>
2019-02-12 18:33:54 -0600

    Fixing syntax issues in models.py generated initial data models

--- chat_data/models.py
+++ chat_data/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
-class MessageAuthor(models.Model)
+class MessageAuthor(models.Model):
     user_id = models.IntegerField()
     first_name = models.CharField(max_length=120)
     last_name = models.CharField(max_length=120)
     image_icon_url = models.URLField()
 
-class Message
+class Message(models.Model):
     author_id = models.IntegerField()
     message_parent_id = models.IntegerField()
     message_body = models.TextField(max_length=1024)
