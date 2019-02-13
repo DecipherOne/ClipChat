38abcd5b011a8fc6f6953b409fd30d8ad9adcd61
Will <willc@decipherone.com>
2019-02-12 18:25:18 -0600

    Setting up project and chat_data app

---
+++ chat_data/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ChatDataConfig(AppConfig):
+    name = 'chat_data'
+    verbose_name = "Chat Data"
---
+++ chat_data/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class MessageAuthor(models.Model)
+    user_id = models.IntegerField()
+    first_name = models.CharField(max_length=120)
+    last_name = models.CharField(max_length=120)
+    image_icon_url = models.URLField()
+
+class Message
+    author_id = models.IntegerField()
+    message_parent_id = models.IntegerField()
+    message_body = models.TextField(max_length=1024)
+    message_id = models.IntegerField()
