d486037492dad4524a634110291f47c4b5b258df
Will <willc@decipherone.com>
2019-02-13 23:02:14 -0600

    Refactoring tests.py to pull out login functionality into reusable method

--- chat_data/tests.py
+++ chat_data/tests.py
@@ -8,6 +8,15 @@ class Setup_Class(TestCase):
 	def setUp(self):
 		self.client = Client()
 		self.User = get_user_model()
+		
+	def loginSuperUser(self):
+		self.userInstance = self.User.objects.create(username ='superUser001', password = '12345PW', is_active = True, is_staff=True, is_superuser=True)
+		self.userInstance.set_password('111Super$')
+		self.userInstance.save()
+		self.userInstance = authenticate(username='superUser001', password = '111Super$')
+		login = self.client.login(username='superUser001', password = '111Super$')
+		return self.userInstance
+	
 
 class TestViews(Setup_Class):
 	"Testing views behave as expected."
@@ -25,12 +34,7 @@ class TestViews(Setup_Class):
 		
 	def test_redirect_to_chat_index_when_user_authenticates(self):
 		
-		#authenticate and login user
-		self.userInstance = self.User.objects.create(username ='superUser001', password = '12345PW', is_active = True, is_staff=True, is_superuser=True)
-		self.userInstance.set_password('111Super$')
-		self.userInstance.save()
-		self.userInstance = authenticate(username='superUser001', password = '111Super$')
-		login = self.client.login(username='superUser001', password = '111Super$')
+		self.loggedInSuperUser = self.loginSuperUser()
 		response = self.client.get("/")
 		user = auth.get_user(self.client)
 		
@@ -39,6 +43,8 @@ class TestViews(Setup_Class):
 		else :
 			self.assertTrue(False,"Could not login user for test.")
 		
+		
+	#def test_profile_and_chat_text_box_render(self):
 	
 
 # Create your tests here.
